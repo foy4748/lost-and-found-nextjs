efdb7cb7d26803b342916163c5bb5dedf2e32f89
foy4748 <faisaljfcl@gmail.com>
2024-08-20 17:00:14 +0600

    Removted origin headers

--- src/redux/apiSlices/baseApiSlice.ts
+++ src/redux/apiSlices/baseApiSlice.ts
@@ -21,10 +21,6 @@ const baseQuery = fetchBaseQuery({
       headers.set("authorization", token);
     }
 
-    headers.set(
-      "Access-Control-Allow-Origin",
-      "https://apollo-assignment-09.vercel.app"
-    );
     return headers;
   },
 });
